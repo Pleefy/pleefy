2bb40a2f7807ce8d08852c990a1302bb8cc2c7e1
Pleefy <info@pleefy.com>
2025-08-24 13:54:42 +0200

    Delete components/ui/textarea.js

--- components/ui/textarea.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { cn } from "./cn";
-export const Textarea = React.forwardRef(({ className, ...props }, ref) => (
-  <textarea ref={ref} className={cn("input min-h-[120px]", className)} {...props} />
-));
-Textarea.displayName = "Textarea";
